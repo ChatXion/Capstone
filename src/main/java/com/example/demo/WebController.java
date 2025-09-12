53cca1c5d64dd16131a4aabeaa1f89e8c14917d4
Chatchai Xiong <gwxiong_2@hotmail.com>
2025-09-11 19:44:49 -0500

    Files uploaded

---
+++ src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String home() {
+        System.out.println("Home controller hit");
+        return "home";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("Test controller hit");
+        return "home";
+    }
+}
